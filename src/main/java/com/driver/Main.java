788b2fb5707cb2fa4290c41c6207dcc13f217bf8
Shrayansshjain <Shrayansshjain@gmail.com>
2024-02-11 13:58:30 +0530

    product class made

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,26 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args){
+        Product p = new Product();
+        int ans = p.product(50,20);
+        int ans2 = p.product(50,20,10);
+        double ans3 = p.product(5.555, 6.6666);
+
+        System.out.print(ans+" "+ ans2+" "+ans3);
+    }
+
+    static class Product{
+
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+
+    }
 }
